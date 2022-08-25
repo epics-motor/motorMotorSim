726d181fb9680d1843c848500a5e4903475374f8
Mark Rivers <rivers@cars.uchicago.edu>
2022-08-25 14:59:46 -0500

    Change to model 3 driver

--- iocs/motorSimIOC/iocBoot/iocMotorSim/st.cmd
+++ iocs/motorSimIOC/iocBoot/iocMotorSim/st.cmd
@@ -1,20 +1,26 @@
-#!../../bin/linux-x86_64/motorSim
+# The is the ASYN example for communication to 4 simulated motors
+# "#!" marks lines that can be uncommented.
 
 < envPaths
 
-cd "${TOP}"
+dbLoadDatabase("$(TOP)/dbd/motorSim.dbd")
+motorSim_registerRecordDeviceDriver(pdbbase)
 
-## Register all support components
-dbLoadDatabase "dbd/motorSim.dbd"
-motorSim_registerRecordDeviceDriver pdbbase
+dbLoadTemplate("motor.substitutions")
 
-cd "${TOP}/iocBoot/${IOC}"
+motorSimCreateController("motorSim1", 8)
+#asynSetTraceIOMask("motorSim1", 0, HEX)
+#asynSetTraceMask("motorSim1", 0, ERROR|FLOW|DRIVER)
 
-
-## motorUtil (allstop & alldone)
-dbLoadRecords("$(MOTOR)/db/motorUtil.db", "P=motorSim:")
-
-##
+# motorSimConfigAxis(port, axis, lowLimit, highLimit, home, start)
+motorSimConfigAxis("motorSim1", 0, 20000, -20000,  500, 0)
+motorSimConfigAxis("motorSim1", 1, 20000, -20000, 1500, 0)
+motorSimConfigAxis("motorSim1", 2, 20000, -20000, 2500, 0)
+motorSimConfigAxis("motorSim1", 3, 20000, -20000, 3000, 0)
+motorSimConfigAxis("motorSim1", 4, 20000, -20000,  500, 0)
+motorSimConfigAxis("motorSim1", 5, 20000, -20000, 1500, 0)
+motorSimConfigAxis("motorSim1", 6, 20000, -20000, 2500, 0)
+motorSimConfigAxis("motorSim1", 7, 20000, -20000, 3000, 0)
 
 iocInit
 
@@ -22,3 +28,4 @@ iocInit
 motorUtilInit("motorSim:")
 
 # Boot complete
+
