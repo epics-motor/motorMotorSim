60b7d40f0eae4406a794a2ac9be6cb933ef698cd
Kevin Peterson <kmpeters@anl.gov>
2023-04-10 13:46:08 -0500

    Multiple modifications to the example IOC config:
    1. Moved the motorSim config to motorSim.cmd, which is now used by both st.cmd and st.cmd.Vx.
    2. Made the IOC prefix in st.cmd consistent with the substitutions file.
    3. Renamed motor.substitutions to motorSim.substitutions.

---
+++ iocs/motorSimIOC/iocBoot/iocMotorSim/motorSim.cmd
@@ -0,0 +1,15 @@
+dbLoadTemplate("motorSim.substitutions")
+
+motorSimCreateController("motorSim1", 8)
+#asynSetTraceIOMask("motorSim1", 0, HEX)
+#asynSetTraceMask("motorSim1", 0, ERROR|FLOW|DRIVER)
+
+# motorSimConfigAxis(port, axis, lowLimit, highLimit, home, start)
+motorSimConfigAxis("motorSim1", 0, 20000, -20000,  500, 0)
+motorSimConfigAxis("motorSim1", 1, 20000, -20000, 1500, 0)
+motorSimConfigAxis("motorSim1", 2, 20000, -20000, 2500, 0)
+motorSimConfigAxis("motorSim1", 3, 20000, -20000, 3000, 0)
+motorSimConfigAxis("motorSim1", 4, 20000, -20000,  500, 0)
+motorSimConfigAxis("motorSim1", 5, 20000, -20000, 1500, 0)
+motorSimConfigAxis("motorSim1", 6, 20000, -20000, 2500, 0)
+motorSimConfigAxis("motorSim1", 7, 20000, -20000, 3000, 0)
