32547ab47953e08ef06ed291cd699444af1e7987
Kevin Peterson <kmpeters@anl.gov>
2023-04-10 14:51:03 -0500

    Added a generic 8-axis iocsh file for applications where all the axes can start with identical configurations

--- iocs/motorSimIOC/iocBoot/iocMotorSim/st.cmd
+++ iocs/motorSimIOC/iocBoot/iocMotorSim/st.cmd
@@ -6,7 +6,10 @@
 dbLoadDatabase("$(TOP)/dbd/motorSim.dbd")
 motorSim_registerRecordDeviceDriver(pdbbase)
 
+# Use a local configuration if axes need individual customization
 < motorSim.cmd
+# Use a default, 8-axis configuration if axes can start with identical settings
+#!< motorSim.iocsh
 
 iocInit
 
